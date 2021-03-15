14ef6cdc6702d098b27b2807b2259ac497b6cd9a
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2021-03-15 21:20:46 +0300

    Add files via upload

---
+++ Массивы/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter array legth");
+        int a = in.nextInt();
+        int array[] = new int[a];
+        System.out.println("Enter array legth");
+        System.out.println("Enter min legth");
+        int min = array[2];
+        for (int b = 2; b < a; b++) {
+            if(array<min) {
+                min = array[b];
+                {
+                }
+                System.out.println("min");
+            }
+        }
+    }
+}
